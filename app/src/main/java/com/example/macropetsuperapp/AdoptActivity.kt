3c8b4189bbdb1b6f25a2aa7ba1bf333fd4425635
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-11-16 14:35:52 +0700

    Nambahin navigation drawer

--- app/src/main/java/com/example/macropetsuperapp/AdoptActivity.kt
+++ app/src/main/java/com/example/macropetsuperapp/AdoptActivity.kt
@@ -3,10 +3,25 @@ package com.example.macropetsuperapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class AdoptActivity : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adopt)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.nav_drawer_layout)
+        val navDrawView : NavigationView = findViewById(R.id.nav_drawer_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
