effe25981a1c204b7a49c09fc51a564d279268d2
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-11-16 13:26:32 +0700

    Nambahin toolbar sedikit

--- app/src/main/java/com/example/macropetsuperapp/AdoptActivity.kt
+++ app/src/main/java/com/example/macropetsuperapp/AdoptActivity.kt
@@ -2,6 +2,7 @@ package com.example.macropetsuperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class AdoptActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/macropetsuperapp/MainActivity.kt
+++ app/src/main/java/com/example/macropetsuperapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.macropetsuperapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.macropetsuperapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
