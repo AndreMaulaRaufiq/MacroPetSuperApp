021d443cdbbec022d795f81921da379994cc7435
PinoyBloon <ikhlazoelzephyr@gmail.com>
2022-11-16 13:03:00 +0700

    Edit home activity dan nambah adopt activity

--- app/src/main/java/com/example/macropetsuperapp/HomeActivity.kt
+++ app/src/main/java/com/example/macropetsuperapp/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.macropetsuperapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.macropetsuperapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.adoptBtn.setOnClickListener {
+            startActivity(Intent(this, AdoptActivity::class.java))
+        }
     }
 }
